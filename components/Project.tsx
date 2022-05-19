058d87806fc2cf5042ce03da0a64c660bb6936f6
Seyf <seyfettin.baskara@gmail.com>
2022-05-19 11:51:52 +0200

    add url to projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,12 +2,34 @@ import React from 'react'
 import styles from '../styles/Projects.module.css'
 import Image from 'next/image'
 import linkedinIcon from '../public/images/linkedin-icon.png'
+import gardenjsIcon from '../public/images/gardenjs.png'
 
 const Project: React.FC = () => {
    return (
       <article className={styles.project}>
          <h1 className={styles.project__title}>My Projects</h1>
          <section className={styles.project__cards}>
+            <div className={styles.project__card}>
+               <Image src={gardenjsIcon} alt="linkedin icon" width={400} height={195} />
+
+               <h3 className={styles.card__title}>Garden JS</h3>
+               <p className={styles.card__description}>
+                  This is team project built with React, Redux, Sass, Node, Express, JWT and MangoDB.
+               </p>
+
+               <ul className={styles.project__links}>
+                  <li className={styles.project__link}>
+                     <a href="https://gardenproject-client.herokuapp.com/" target="_blank" rel="noreferrer">
+                        Live Demo
+                     </a>
+                  </li>
+                  <li className={styles.project__link}>
+                     <a href="https://github.com/SeyfBaskara/garden-js" target="_blank" rel="noreferrer">
+                        Github Link
+                     </a>
+                  </li>
+               </ul>
+            </div>
             <div className={styles.project__card}>
                <Image src={linkedinIcon} alt="linkedin icon" width={150} height={150} />
 
@@ -18,27 +40,14 @@ const Project: React.FC = () => {
 
                <ul className={styles.project__links}>
                   <li className={styles.project__link}>
-                     <a href="#">Live Demo</a>
+                     <a href="#" target="_blank" rel="noreferrer">
+                        Live Demo
+                     </a>
                   </li>
                   <li className={styles.project__link}>
-                     <a href="#">Github Link</a>
-                  </li>
-               </ul>
-            </div>
-            <div className={styles.project__card}>
-               <Image src={linkedinIcon} alt="linkedin icon" width={150} height={150} />
-
-               <h3 className={styles.card__title}>GardenJS Project</h3>
-               <p className={styles.card__description}>
-                  This is team project built with React, Redux, Nodejs, ExpressJS, JWT and MangoDB.
-               </p>
-
-               <ul className={styles.project__links}>
-                  <li className={styles.project__link}>
-                     <a href="#">Live Demo</a>
-                  </li>
-                  <li className={styles.project__link}>
-                     <a href="#">Github Link</a>
+                     <a href="https://github.com/SeyfBaskara/LinkedIn-clone" target="_blank" rel="noreferrer">
+                        Github Link
+                     </a>
                   </li>
                </ul>
             </div>

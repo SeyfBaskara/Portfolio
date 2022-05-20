e7cb1c71d128870e2445868480883bdc04e3a0a2
Seyf <seyfettin.baskara@gmail.com>
2022-05-20 15:10:32 +0200

    fetch project content from json

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,56 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Projects.module.css'
 import Image from 'next/image'
-import linkedinIcon from '../public/images/linkedin-icon.png'
-import gardenjsIcon from '../public/images/gardenjs.png'
+import customData from '../data/data.json'
+
+interface Projects {
+   projects: Array<{
+      title: string
+      description: string
+      githubUrl: string
+      liveUrl: string
+      img: string
+      id: string
+   }>
+}
 
 const Project: React.FC = () => {
+   const [data, setData] = useState<Projects>(customData)
+
    return (
       <article className={styles.project}>
          <h1 className={styles.project__title}>My Projects</h1>
          <section className={styles.project__cards}>
-            <div className={styles.project__card}>
-               <Image src={gardenjsIcon} alt="linkedin icon" width={400} height={195} />
+            {data.projects.map((project) => {
+               return (
+                  <div key={project.id} className={styles.project__card}>
+                     <Image src={`/images/${project.img}.png`} alt="linkedin icon" width={400} height={195} />
+                     <h3 className={styles.card__title}>{project.title}</h3>
+                     <p className={styles.card__description}>{project.description}</p>
 
-               <h3 className={styles.card__title}>Garden JS</h3>
-               <p className={styles.card__description}>
-                  This is team project built with React, Redux, Sass, Node, Express, JWT and MangoDB.
-               </p>
-
-               <ul className={styles.project__links}>
-                  <li className={styles.project__link}>
-                     <a href="https://gardenproject-client.herokuapp.com/" target="_blank" rel="noreferrer">
-                        Live Demo
-                     </a>
-                  </li>
-                  <li className={styles.project__link}>
-                     <a href="https://github.com/SeyfBaskara/garden-js" target="_blank" rel="noreferrer">
-                        Github Link
-                     </a>
-                  </li>
-               </ul>
-            </div>
-            <div className={styles.project__card}>
-               <Image src={linkedinIcon} alt="linkedin icon" width={150} height={150} />
-
-               <h3 className={styles.card__title}>LinkedIn Clone</h3>
-               <p className={styles.card__description}>
-                  This project built with React, Redux and Firestore. Mostly focus on layout of LinkedIn.
-               </p>
-
-               <ul className={styles.project__links}>
-                  <li className={styles.project__link}>
-                     <a href="https://github.com/SeyfBaskara/LinkedIn-clone" target="_blank" rel="noreferrer">
-                        Live Demo
-                     </a>
-                  </li>
-                  <li className={styles.project__link}>
-                     <a href="https://github.com/SeyfBaskara/LinkedIn-clone" target="_blank" rel="noreferrer">
-                        Github Link
-                     </a>
-                  </li>
-               </ul>
-            </div>
+                     <ul className={styles.project__links}>
+                        <li className={styles.project__link}>
+                           <a href={project.liveUrl} target="_blank" rel="noreferrer">
+                              Live Demo
+                           </a>
+                        </li>
+                        <li className={styles.project__link}>
+                           <a href={project.githubUrl} target="_blank" rel="noreferrer">
+                              Github Link
+                           </a>
+                        </li>
+                     </ul>
+                  </div>
+               )
+            })}
          </section>
       </article>
    )

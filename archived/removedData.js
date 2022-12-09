969a9b8dfd48431952b02e049964b18eb2c6d8be
Seyf <seyfettin.baskara@gmail.com>
2022-12-09 14:59:04 +0100

    remove uncomplete project from data

---
+++ archived/removedData.js
@@ -0,0 +1,8 @@
+// {
+//     "title": "LinkedIn Clone ",
+//     "description": "This project built with React, Redux and Firestore. Mostly focus on layout of LinkedIn.",
+//     "githubUrl": "https://github.com/SeyfBaskara/LinkedIn-clone",
+//     "liveUrl": "https://github.com/SeyfBaskara/LinkedIn-clone",
+//     "img": "linkedinicon",
+//     "id": "3"
+//  }

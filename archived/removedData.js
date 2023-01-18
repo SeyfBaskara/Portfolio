066bad2b9f771677c470fb69c070b8fdd8e1cf38
Seyf <seyfettin.baskara@gmail.com>
2023-01-18 13:40:31 +0100

    ignore archived folder

--- archived/removedData.js
+++
@@ -1,8 +0,0 @@
-// {
-//     "title": "LinkedIn Clone ",
-//     "description": "This project built with React, Redux and Firestore. Mostly focus on layout of LinkedIn.",
-//     "githubUrl": "https://github.com/SeyfBaskara/LinkedIn-clone",
-//     "liveUrl": "https://github.com/SeyfBaskara/LinkedIn-clone",
-//     "img": "linkedinicon",
-//     "id": "3"
-//  }

9bcc7ed1dd9ca76145a2f0583093de08e1aecdfa
Seyf <seyfettin.baskara@gmail.com>
2023-02-17 16:26:26 +0100

    add lorem ipsum

--- components/Profile/AboutMe.tsx
+++ components/Profile/AboutMe.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AboutMe = (): JSX.Element => {
+   const [showText, setShowText] = useState<boolean>(false)
+
    return (
       <section className="p-4 md:w-11/12 md:m-auto lg:w-4/5">
-         <div className="text-center mt-3">
+         <div className="flex flex-col items-center gap-5 mt-3">
             <button
-               onClick={() => console.log('about me')}
-               className="font-semibold tracking-widest border-b-2 border-Green pb-1 outline-none hover:text-Green focus:text-Green "
+               onClick={() => setShowText(!showText)}
+               className="font-semibold tracking-widest border-b-2 border-Green pb-1 outline-none hover:text-Green focus:text-Green"
             >
                ABOUT ME
             </button>
-            <p>some text...</p>
+            {showText && (
+               <p className="text-base">
+                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                  alteration in some form, by injected humour, or randomised words which dont look even slightly
+                  believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything
+                  embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to
+                  repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a
+                  dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate
+                  Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from
+                  repetition, injected humour, or non-characteristic words etc.
+               </p>
+            )}
          </div>
       </section>
    )

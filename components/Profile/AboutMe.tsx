698664510615b21ed226ab5a7128757402684722
Seyf <seyfettin.baskara@gmail.com>
2023-02-19 14:55:45 +0100

    add heroicon and headless ui

--- components/Profile/AboutMe.tsx
+++ components/Profile/AboutMe.tsx
@@ -1,21 +1,37 @@
 import React, { useState } from 'react'
+import { ChevronDoubleDownIcon, ChevronDoubleUpIcon } from '@heroicons/react/24/solid'
+import { Transition } from '@headlessui/react'
 
 const AboutMe = (): JSX.Element => {
-   const [showText, setShowText] = useState<boolean>(false)
+   const [isShowing, setIsShowing] = useState<boolean>(false)
+   const [isAnimate, setIsAnimate] = useState<boolean>(false)
 
    return (
       <section className="p-4 md:w-11/12 md:m-auto lg:w-4/5">
          <div className="flex flex-col items-center gap-5 mt-3">
             <button
-               onClick={() => setShowText(!showText)}
-               className="font-semibold tracking-widest border-b-2 border-Green pb-1 outline-none hover:text-Green focus:text-Green"
+               onClick={() => setIsShowing(!isShowing)}
+               onMouseEnter={() => setIsAnimate(true)}
+               onMouseLeave={() => setIsAnimate(false)}
+               className="flex flex-col items-center font-semibold tracking-widest hover:text-Green focus:text-Green"
             >
                ABOUT ME
+               {isShowing ? (
+                  <ChevronDoubleUpIcon className={`w-6 h-6 ${isAnimate && 'animate-bounce'} `} />
+               ) : (
+                  <ChevronDoubleDownIcon className={`w-6 h-6 ${isAnimate && 'animate-bounce'} `} />
+               )}
             </button>
-            <p
-               className={`text-base md:w-4/5 transition-all ease-in-out ${
-                  showText ? 'h-0' : 'h-auto'
-               } overflow-hidden`}
+            <Transition
+               show={isShowing}
+               as="p"
+               enter="transition-opacity duration-75"
+               enterFrom="opacity-0"
+               enterTo="opacity-100"
+               leave="transition-opacity duration-150"
+               leaveFrom="opacity-100"
+               leaveTo="opacity-0"
+               className="text-base  md:w-4/5 "
             >
                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
                in some form, by injected humour, or randomised words which dont look even slightly believable. If you
@@ -25,7 +41,7 @@ const AboutMe = (): JSX.Element => {
                words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
                reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or
                non-characteristic words etc.
-            </p>
+            </Transition>
          </div>
       </section>
    )

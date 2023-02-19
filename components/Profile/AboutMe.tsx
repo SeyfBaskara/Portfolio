d64347d467720a747cfcc6df9cc0675261cfd4b5
Seyf <seyfettin.baskara@gmail.com>
2023-02-19 13:07:50 +0100

    add temperary transition

--- components/Profile/AboutMe.tsx
+++ components/Profile/AboutMe.tsx
@@ -12,18 +12,20 @@ const AboutMe = (): JSX.Element => {
             >
                ABOUT ME
             </button>
-            {showText && (
-               <p className="text-base">
-                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered
-                  alteration in some form, by injected humour, or randomised words which dont look even slightly
-                  believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything
-                  embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to
-                  repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a
-                  dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate
-                  Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from
-                  repetition, injected humour, or non-characteristic words etc.
-               </p>
-            )}
+            <p
+               className={`text-base md:w-4/5 transition-all ease-in-out ${
+                  showText ? 'h-0' : 'h-auto'
+               } overflow-hidden`}
+            >
+               There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
+               in some form, by injected humour, or randomised words which dont look even slightly believable. If you
+               are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in
+               the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as
+               necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin
+               words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
+               reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or
+               non-characteristic words etc.
+            </p>
          </div>
       </section>
    )

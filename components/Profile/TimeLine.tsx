dda989f513ff369c40d3d5520263b1c435e36abd
Seyf <seyfettin.baskara@gmail.com>
2023-02-24 16:25:46 +0100

    add experince and education components

--- components/Profile/TimeLine.tsx
+++ components/Profile/TimeLine.tsx
@@ -1,74 +1,40 @@
 import React from 'react'
 import Image from 'next/image'
+import WorkExperience from './WorkExperience'
+import Education from './Education'
 
 const TimeLine = (): JSX.Element => {
+   const handleShowButton = () => {
+      console.log('show button')
+   }
+
    return (
       <section className="p-4 md:w-11/12 md:m-auto lg:w-4/5">
-         <div className="text-white">
-            <div className="container  flex flex-col items-start md:flex-row my-12 md:my-24">
-               <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 pr-6">
-                  <p className="ml-2 text-yellow-300 uppercase tracking-loose">Working Process</p>
-                  <p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">Working Process of Fest</p>
-                  <p className="text-sm md:text-base text-gray-50 mb-4">
-                     Here’s your guide to the tech fest 2021 process. Go through all the steps to know the exact process
-                     of the fest.
-                  </p>
-               </div>
-               <div className="mt-10 ml-0 md:mt-0 md:ml-12 lg:w-2/3 sticky">
-                  <div className="container mx-auto w-full h-full">
-                     <div className="relative wrap overflow-hidden h-full">
-                        <div className="border-2-2 border-yellow-555 absolute h-full border-2 border-TimelineBorder right-1/2 "></div>
-                        <div className="border-2-2 border-yellow-555 absolute h-full border-2 border-TimelineBorder left-1/2 "></div>
+         <div className="flex flex-col items-center my-12 md:items-start md:flex-row md:my-24">
+            <header className="flex flex-col items-center max-w-sm gap-3 mb-10 sticky md:items-start md:mb-0 md:top-36 md:pr-6 lg:w-1/3">
+               <h1 className="text-large font-bold">Timeline</h1>
+               <p className="text-base ">
+                  Timeline present my work experiences. Click show button to preview education background.
+               </p>
+               <button
+                  onClick={handleShowButton}
+                  className="font-semibold tracking-widest border-b-2 border-Green pb-1 outline-none hover:text-Green focus:text-Green "
+               >
+                  SHOW EDUCATION
+               </button>
+            </header>
 
-                        <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                           <div className="order-1 w-5/12"></div>
-                           <div className="order-1 w-5/12 px-1 py-4 text-right">
-                              <p className="mb-3 text-base text-yellow-300">1-6 May, 2021</p>
-                              <h4 className="mb-3 font-bold text-lg md:text-2xl">Registration</h4>
-                              <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                                 Pick your favourite event(s) and register in that event by filling the form
-                                 corresponding to that event. Its that easy :)
-                              </p>
-                           </div>
-                        </div>
-                        <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                           <div className="order-1 w-5/12"></div>
-                           <div className="order-1  w-5/12 px-1 py-4 text-left">
-                              <p className="mb-3 text-base text-yellow-300">6-9 May, 2021</p>
-                              <h4 className="mb-3 font-bold text-lg md:text-2xl">Participation</h4>
-                              <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                                 Participate online. The links for your registered events will be sent to you via email
-                                 and whatsapp groups. Use those links and show your talent.
-                              </p>
-                           </div>
-                        </div>
-                        <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                           <div className="order-1 w-5/12"></div>
-                           <div className="order-1 w-5/12 px-1 py-4 text-right">
-                              <p className="mb-3 text-base text-yellow-300"> 10 May, 2021</p>
-                              <h4 className="mb-3 font-bold text-lg md:text-2xl">Result Declaration</h4>
-                              <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                                 The ultimate genius will be revealed by our judging panel on 10th May, 2021 and the
-                                 resukts will be announced on the whatsapp groups and will be mailed to you.
-                              </p>
-                           </div>
-                        </div>
+            <div className="mt-10 ml-0 md:mt-0 md:ml-12 lg:w-2/3 sticky">
+               <div className="container mx-auto w-full h-full">
+                  <div className="relative wrap overflow-hidden h-full">
+                     <div className="border-2-2 border-yellow-555 absolute h-full border-2 border-Green right-1/2 "></div>
+                     <div className="border-2-2 border-yellow-555 absolute h-full border-2 border-Green left-1/2 "></div>
 
-                        <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                           <div className="order-1 w-5/12"></div>
-                           <div className="order-1  w-5/12 px-1 py-4">
-                              <p className="mb-3 text-base text-yellow-300">12 May, 2021</p>
-                              <h4 className="mb-3 font-bold  text-lg md:text-2xl text-left">Prize Distribution</h4>
-                              <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                                 The winners will be contacted by our team for their addresses and the winning goodies
-                                 will be sent at their addresses.
-                              </p>
-                           </div>
-                        </div>
-                     </div>
-                     <div className="mx-auto -mt-24 md:-mt-24 md:ml-28">
-                        <Image src="/images/timeline.png" alt="images" width={375} height={258} />
-                     </div>
+                     <WorkExperience />
+                     <Education />
+                  </div>
+                  <div className="mx-auto -mt-24 md:-mt-24 md:ml-28">
+                     <Image src="/images/timeline.png" alt="images" width={375} height={258} />
                   </div>
                </div>
             </div>

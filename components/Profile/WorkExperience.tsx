e6dd0b30bf7baf7c28a3e32d4541d2c889af4505
Seyf <seyfettin.baskara@gmail.com>
2023-02-24 17:22:09 +0100

    fetch experince data from json

--- components/Profile/WorkExperience.tsx
+++ components/Profile/WorkExperience.tsx
@@ -12,28 +12,23 @@ interface IExperinceProps {
 const WorkExperience: React.FC<IExperinceProps> = ({ experience }) => {
    return (
       <section>
-         <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="order-1 w-5/12 px-1 py-4 text-right">
-               <p className="mb-3 text-base text-yellow-300">1-6 May, 2021</p>
-               <h4 className="mb-3 font-bold text-lg md:text-2xl">Registration</h4>
-               <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                  Pick your favourite event(s) and register in that event by filling the form corresponding to that
-                  event. Its that easy :)
-               </p>
-            </div>
-         </div>
-         <div className="mb-8 flex justify-between items-center w-full right-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="order-1  w-5/12 px-1 py-4 text-left">
-               <p className="mb-3 text-base text-yellow-300">6-9 May, 2021</p>
-               <h4 className="mb-3 font-bold text-lg md:text-2xl">Participation</h4>
-               <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                  Participate online. The links for your registered events will be sent to you via email and whatsapp
-                  groups. Use those links and show your talent.
-               </p>
-            </div>
-         </div>
+         <ul>
+            {experience.map((item, index) => (
+               <li
+                  key={item.id}
+                  className={`mb-8 flex justify-between items-center ${
+                     index % 2 == 0 ? 'flex-row-reverse left-timeline' : 'right-timeline'
+                  }`}
+               >
+                  <div className="order-1 w-5/12"></div>
+                  <div className={`order-1 w-5/12 px-1 py-4 ${index % 2 == 0 ? 'text-right' : 'text-left'} `}>
+                     <h4 className="mb-3 text-base text-yellow-300">{item.title}</h4>
+                     <p className="mb-3 font-bold text-lg md:text-2xl">{item.date}</p>
+                     <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">{item.company}</p>
+                  </div>
+               </li>
+            ))}
+         </ul>
       </section>
    )
 }

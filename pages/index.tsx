93a63c8c0b04db034b1a71b7c782f3dae2f3ab64
Seyf <seyfettin.baskara@gmail.com>
2023-11-29 20:15:06 +0100

    (feat) disable about me component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Layout from '../components/Layout'
-import AboutMe from '../components/Profile/AboutMe'
+// import AboutMe from '../components/Profile/AboutMe'
 import Projects from '../components/Projects/Projects'
 import Technologies from '../components/Technologies'
 import data from '../data/data.json'
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
    return (
       <Layout metaData={data.metaData}>
          <>
-            <AboutMe about={data.profile.about} />
+            {/* <AboutMe about={data.profile.about} /> */}
             <Technologies techStack={data.tools} />
             <Projects projects={data.projects} />
          </>
